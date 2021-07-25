e961e5c3512daa5fda79341813d25ef9db53312d
Eduardo Sacramento de Sousa <eduardo.sacra.sousa@gmail.com>
2021-07-25 12:53:00 -0300

    Atualizacao nome do database

--- scripts/03-PROC_CONSULTAR_VIDEOS.sql
+++ scripts/03-PROC_CONSULTAR_VIDEOS.sql
@@ -1,16 +1,13 @@
-
-use AluraFlix;
-
-
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROC CONSULTAR_VIDEOS
+ALTER PROC [dbo].[CONSULTAR_VIDEOS]
 AS
     SELECT [Id]
         ,[Titulo]
         ,[Descricao]
         ,[URL_Video]
-    FROM [VideoManager].[dbo].[Video]
+    FROM [dbo].[Video]
 GO;
+GO
